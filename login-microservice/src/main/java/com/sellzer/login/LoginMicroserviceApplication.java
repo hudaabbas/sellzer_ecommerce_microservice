0908b4ad6a4a378e20fcc4646ded2a6b33046f14
AgamAulakh <aulakhagam@gmail.com>
2022-03-23 14:55:11 -0600

    adds eureka service managergit status runs all services :D TODO: API gateway

--- login-microservice/src/main/java/com/sellzer/login/LoginMicroserviceApplication.java
+++ login-microservice/src/main/java/com/sellzer/login/LoginMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.sellzer.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LoginMicroserviceApplication {
 
 	public static void main(String[] args) {
--- payment-service/src/main/java/com/sellzer/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/sellzer/payment/PaymentServiceApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
