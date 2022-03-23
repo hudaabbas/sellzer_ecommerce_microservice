6d1198e8f2c102d254f0e699bc6fddf6b870ccf5
Lubaba Sheikh <lubaba.sheikh01@gmail.com>
2022-03-23 10:29:28 -0600

    added endpoints

--- login-microservice/src/main/java/com/sellzer/login/service/LoginService.java
+++ login-microservice/src/main/java/com/sellzer/login/service/LoginService.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Service
 @Slf4j
@@ -16,16 +19,36 @@ public class LoginService {
 
     public Login saveLogin(Login login)
     {
-        //log.info("Inside saveDepartment method of LoginService");
+        log.info("Inside saveDepartment method of LoginService");
         return loginRepository.save(login);
     }
 
 
     public Login findLoginByEmail(String email)
     {
-        //log.info("Inside findLoginByEmail method of LoginService");
+        log.info("Inside findLoginByEmail method of LoginService");
         return loginRepository.findByEmail(email);
     }
 
+
+    public Login findLoginById(String loginId) {
+        log.info("Inside findLoginByLoginId method of LoginService");
+        return loginRepository.findByLoginId(loginId);
+    }
+
+    public Map<String, Boolean> deleteLoginById(String loginId) {
+        log.info("Inside deleteLoginById method of LoginService");
+
+        Login login = loginRepository.findByLoginId(loginId);
+        Map<String, Boolean> response = new HashMap<>();
+        if (login == null) {
+            log.info("User not found for :: " + loginId);
+            response.put("Deleted", Boolean.FALSE);
+        } else {
+            loginRepository.delete(login);
+            response.put("Deleted", Boolean.TRUE);
+        }
+        return response;
+    }
 
 }
