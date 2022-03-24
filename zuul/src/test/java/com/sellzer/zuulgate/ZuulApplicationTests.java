027bf22c2717f37c1e810f7850c275086b94f446
AgamAulakh <aulakhagam@gmail.com>
2022-03-23 23:01:08 -0600

    adds zuul gateway for api endpoint routing

---
+++ zuul/src/test/java/com/sellzer/zuulgate/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellzer.zuulgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
