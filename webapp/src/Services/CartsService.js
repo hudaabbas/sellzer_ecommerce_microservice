3bb809f0c1a84e3496293618975e27c0a37942d6
hudaabbas <hudaabbas1234@gmail.com>
2022-03-28 15:08:20 -0600

    add a cart object once a user registers

--- webapp/src/Services/CartsService.js
+++ webapp/src/Services/CartsService.js
@@ -4,20 +4,16 @@ const CARTS_REST_API_URL = 'http://localhost:9003/carts/';
 
 class CartService {
 
-    addCarts(){
+    addCarts(body){
         return axios({
             method: 'post',
             url: CARTS_REST_API_URL,
             withCredentials: false,
             headers: {
                 'Access-Control-Allow-Origin':'*',
-                'Content-Type': 'application/json;charset=UTF-8',
+                'Content-Type': 'application/json;',
             },
-            data: {
-                'userId': 6785439,
-                'products': ["Nike Air Forces", "Puma Socks"],
-                'services': []
-            }
+            data: body
         });
     }
 
