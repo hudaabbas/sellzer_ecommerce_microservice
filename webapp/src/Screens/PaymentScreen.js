b7dbd2978ece9a7f53f2f23dbdee029f0fae9eb2
Nuha <nuha.shaikh@gmail.com>
2022-03-29 12:07:27 -0600

    payment form redirection and error checking

--- webapp/src/Screens/PaymentScreen.js
+++ webapp/src/Screens/PaymentScreen.js
@@ -7,7 +7,7 @@ import CartsService from '../Services/CartsService';
 import { useHistory } from "react-router-dom";
 import '../Login.css';
 class PaymentScreen extends React.Component {
-       // const product= data.products.find( x=> x._id === props.match.params.id);
+    // const product= data.products.find( x=> x._id === props.match.params.id);
     constructor(props){
         console.log(props.match.params.id);
         super(props)
@@ -52,9 +52,19 @@ class PaymentScreen extends React.Component {
         e.preventDefault();
         if (this.validateForm()) 
         {
-            CartsService.clearCart(e, this.props.match.params.id);
-            console.log(data);
-            console.log(this.state);
+            CartsService.clearCart(e, this.props.match.params.id).then((response) => {
+            console.log(response);
+            if(response.status = 200){
+                console.log("Item added successfully");
+                window.location.href = "/confirmation/"+this.state.payments.paymentId;
+            }else{
+                alert(response.statusText);
+            }
+            }).catch(function (error) {
+                console.log(error);
+            });
+                console.log(data);
+                console.log(this.state);
         }
     }
 
@@ -81,18 +91,27 @@ class PaymentScreen extends React.Component {
     validateForm() {
         let errors = {};
         let formIsValid = true;
-        let cardNum = console.log(document. getElementById("cardNumber").value);
-        let exprNum = document. getElementById("expiryDate").value;
-        if (!this.state.cardNumber) {
+        let cardNum = document.getElementById("cardNumber").value;
+        let exprNum = document.getElementById("expiryDate").value;
+        let cvvNum = document.getElementById("CvvNumber").value;
+        let cardName =  document.getElementById("cardHolderName").value;
+        let shippAddr = document.getElementById("shippingAddress").value;
+        console.log(cardNum);
+        console.log(exprNum);
+        console.log(cvvNum);
+        console.log(cardName);
+        console.log(shippAddr);
+        if (!cardNum) 
+        {
             formIsValid = false;
             errors["cardNumber"] = "Please enter your card number";
-        }
-
-        if (typeof this.state.cardNumber !== "undefined") {
-            //regular expression for email validation
-            const numeric = this.state.cardNumber.replace(/\D/g,'');
+        }else if(typeof cardNum !== "undefined") 
+        {
+            const numeric = cardNum.replace(/\D/g,'');
+            const num= parseInt(numeric);
+            console.log(num);
             var pattern = new RegExp(/^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/i);
-            if (!pattern.test(this.state.cardNumber)) {
+            if (!pattern.test(numeric)) {
                 formIsValid = false;
                 errors["cardNumber"] = "Please enter a valid cardNumber.";
             }
@@ -101,27 +120,37 @@ class PaymentScreen extends React.Component {
         if (!exprNum) {
             formIsValid = false;
             errors["expiryDate"] = "Please enter expiry date.";
-        } 
-
-        if(typeof this.state.expiryDate !== "undefined"){
-            //nuha add regex for expiry date
+        } else if(typeof exprNum !== "undefined"){
+            const numeric = exprNum.replace(/\D/g,'');
+            console.log(numeric);
+            var pattern = new RegExp(/^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/i);
+            if (!pattern.test(numeric)) {
+                formIsValid = false;
+                errors["expiryDate"] = "Please enter a valid expiration date.";
+            }
         }
 
-        if (!this.state.CvvNumber) {
+        if (!cvvNum) 
+        {
             formIsValid = false;
             errors["CvvNumber"] = "Please enter cvv number.";
-        } 
-
-        if(typeof this.state.CvvNumber !== "undefined"){
-            //nuha add regex for expiry date
+        } else if(typeof cvvNum !== "undefined")
+        {
+            const numeric = cvvNum .replace(/\D/g,'');
+            const num= parseInt(numeric);
+            var pattern = new RegExp(/^[0-9]{3,4}$/);
+            if (!pattern.test(num)) {
+                formIsValid = false;
+                errors["CvvNumber"] = "Please enter a valid cvv number.";
+            }
         }
 
-        if (!this.state.cardHolderName) {
+        if (!cardName) {
             formIsValid = false;
             errors["cardHolderName"] = "Please enter Card Holder Name.";
         } 
 
-        if (!this.state.shippingAddress) {
+        if (!shippAddr) {
             formIsValid = false;
             errors["shippingAddress"] = "Please enter Shipping Address.";
         } 
@@ -133,8 +162,6 @@ class PaymentScreen extends React.Component {
         return formIsValid;
     }
 
-    
-    
      render() {
         return (
         <html lang="en">
@@ -168,8 +195,8 @@ class PaymentScreen extends React.Component {
                         //class="card-number-field"
                         name = "cardNumber"
                         id="cardNumber"
-                        placeholder="###-###-###"
-                        required minLength="9" maxLength="9"
+                        placeholder="#################"
+                        required minLength="11" maxLength="19"
                         onChange={this.handleChangeCardNumber} 
                     />
                     <div className="errorMsg">{this.state.errors.cardNumber}</div>
@@ -179,8 +206,8 @@ class PaymentScreen extends React.Component {
                 <div class="date-number">
                     <label className="payment-text"> Expiry Date </label>
                     <input type="text" class="date-number-field" 
-                        placeholder="DD-MM-YY" 
-                        required minLength="6" maxLength="6"
+                        placeholder="MM/YY" 
+                        required minLength="8" maxLength="8"
                         id="expiryDate"
                         onChange={this.handleExpiryDate} 
                     />
@@ -191,8 +218,8 @@ class PaymentScreen extends React.Component {
                     <label className="payment-text"> CVV number </label>
                     <input type="text" class="cvv-number-field" 
                         placeholder="xxx" 
+                        id="CvvNumber"
                         required minLength="3" maxLength="3"
-                        value={this.state.CvvNumber} 
                         onChange={this.handleCvvNumber} 
                     />
                     <div className="errorMsg">{this.state.errors.CvvNumber}</div>
@@ -202,9 +229,8 @@ class PaymentScreen extends React.Component {
                     <label className="payment-text"> Card Holder name </label>
                     <input
                         type="text"
-                        //class="card-name-field"
+                        id="cardHolderName"
                         placeholder="Enter your Name"
-                        value={this.state.cardHolderName} 
                         onChange={this.handleCardHolderName}
                     />
                     <div className="errorMsg">{this.state.errors.cardHolderName}</div>
@@ -214,14 +240,13 @@ class PaymentScreen extends React.Component {
                     <label className="payment-text"> Shipping Address </label>
                     <input
                         type="text"
-                        //class="card-name-field"
+                        id="shippingAddress"
                         placeholder="Enter your Address"
-                        value={this.state.shippingAddress} 
                         onChange={this.handleShippingAddress}
                     />
                     <div className="errorMsg">{this.state.errors.shippingAddress}</div>
                 </div>
-            <Link to={"/confirmation/"+this.state.payments.paymentId} className="btn btn-primary" onClick={(e) =>this.submituserPaymentForm(e)}>Checkout</Link>
+            <Link className="btn btn-primary" onClick={(e) =>this.submituserPaymentForm(e)} to={"/confirmation/"+this.state.payments.paymentId}>Checkout</Link>
             </div>
             </div>
         </body>
